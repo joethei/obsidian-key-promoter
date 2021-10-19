112510372bfa083db74f9ef51ca9baf1e772615e
Johannes Theiner <kontakt@joethei.xyz>
2021-10-19 09:54:58 +0200

    - Added threshold to max number of commands to show - don't show notifications if clicking on files with matching names
    Fixes #5
    Fixes #6

--- main.ts
+++ main.ts
@@ -3,11 +3,13 @@ import {Command, Hotkey, Notice, Platform, Plugin, PluginSettingTab, Setting} fr
 interface KeyPromoterSettings {
 	showUnassigned: boolean;
 	showAssigned: boolean;
+	threshold: number;
 }
 
 const DEFAULT_SETTINGS: KeyPromoterSettings = {
 	showUnassigned: true,
-	showAssigned: true
+	showAssigned: true,
+	threshold: 0,
 }
 
 
@@ -31,9 +33,14 @@ export default class KeyPromoterPlugin extends Plugin {
 			if(!label) label = event.target.innerText;
 			if(!label) return;
 
-			//ignore all clicks that happen in settings
 			//@ts-ignore
-			if(event.target.offsetParent && event.target.offsetParent.classList.contains("mod-settings")) return;
+			const offsetParent = event.target.offsetParent;
+
+			//don't show notifications when in settings, file explorer, etc.
+			if(offsetParent) {
+				if(offsetParent.classList.contains("mod-settings")) return;
+				if(offsetParent.classList.contains("nav-files-container")) return;
+			}
 
 			//@ts-ignore
 			let commands: Command[] = Object.values(this.app.commands.commands);
@@ -44,17 +51,22 @@ export default class KeyPromoterPlugin extends Plugin {
 				 */
 				return command.name.toLowerCase().contains(label.toLowerCase());
 			});
+			if(this.settings.threshold != 0 && commands.length > this.settings.threshold) {
+				new Notice("there are to many hotkeys that could fit the action named \"" + label +  "\"");
+				return;
+			}
+
 			commands.forEach((command) => {
 				if(command.hotkeys == undefined) {
 					if(this.settings.showUnassigned) {
-						new Notice("Hotkey for " + command.name + " is not set");
+						new Notice("Hotkey for \"" + command.name + "\" is not set");
 					}
 					return;
 				}
 				if(this.settings.showAssigned) {
 					command.hotkeys.forEach((hotkey: Hotkey) => {
 						const modifiers = hotkey.modifiers.join("+").replace('Mod',  Platform.isMacOS ? 'Cmd' : 'Ctrl');
-						new Notice("Hotkey for " + command.name + " is " + modifiers + " + " + hotkey.key);
+						new Notice("Hotkey for \"" + command.name + "\" is \"" + modifiers + " + " + hotkey.key + "\"");
 					});
 				}
 			});
@@ -111,6 +123,23 @@ class KeyPromoterSettingsTab extends PluginSettingTab {
 						this.plugin.settings.showUnassigned = value;
 						await this.plugin.saveSettings();
 					});
+			});
+
+		new Setting(containerEl)
+			.setName('Threshold')
+			.setDesc('Only show notification if there are less than X possible commands (use 0 to disable)')
+			.addText(text => {
+				text
+					.setValue(String(this.plugin.settings.threshold))
+					.onChange(async (value) => {
+						if(isNaN(Number(value)) || value === undefined) {
+							new Notice("please specify a valid number");
+							return;
+						}
+						this.plugin.settings.threshold = Number(value);
+						await this.plugin.saveSettings();
+					});
+
 			});
 	}
 }
